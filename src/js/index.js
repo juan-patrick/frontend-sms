079ae30d2769c8bb94d81e0fda18b861cab0d0c8
juan-patrick <juan-p.andrade@outlook.com>
2021-03-09 23:51:52 -0300

    feat: add sms form and phone validate

--- src/js/index.js
+++ src/js/index.js
@@ -11,3 +11,41 @@ const Navigation = {
     this.sections.map(section => document.getElementById(section).classList.remove('hidden'));
   }
 }
+
+
+const Utils = {
+  formatPhone(phone) {
+    let phoneDigits = phone.replace(/\D/g, '');
+    const phoneRegex = new RegExp('^((1[1-9])|([2-9][0-9]))((3[0-9]{3}[0-9]{4})|(9[0-9]{3}[0-9]{5}))$');
+    return phoneRegex.test(phoneDigits);
+  }
+}
+
+const FormSMS = {
+  phone: document.querySelector('input#phone'),
+  message: document.querySelector('textarea#message'),
+  getValues() {
+    return {
+      phone: this.phone.value,
+      message: this.message.value
+    }
+  },
+  clearFields() {
+    this.message.value = "";
+    this.phone.value = "";
+  },
+  validateFields() {
+    const { phone, message } = FormSMS.getValues();
+
+    console.log(Utils.formatPhone(phone));
+  },
+  submit(event) {
+    event.preventDefault();
+    try {
+      FormSMS.validateFields();
+      FormSMS.clearFields();
+    } catch (error) {
+      alert(error.message);
+    }
+  }
+}
